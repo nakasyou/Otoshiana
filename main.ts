6dac73d43331c48d79d1a9ea75da4c2c5bfcc411
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-05-22 16:13:32 +0900

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,8 @@
+import { serve } from 'https://deno.land/std@0.188.0/http/server.ts'
+import { Hono } from 'https://deno.land/x/hono@v3.2.1/mod.ts'
+
+const app = new Hono()
+
+app.get('/', (c) => c.text('Hello! Hono!'))
+
+serve(app.fetch)
